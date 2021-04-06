3dbf23942e094e23744224ea67f58a6c976dbbbe
T <consolelogger@fastmail.fm>
2021-04-06 14:07:23 +0300

    Added my project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../css/App.css';
+const Header = () => {
+    return (
+        <div>
+         <h1>
+          REACT <br/> PORTFOLIO <br/> 2021
+        </h1>
+        </div>
+    )
+}
+
+export default Header
